abe8335eea7cc1f667dbc0eac6dea356df8f4bd6
madalinap22 <popescumadalina.gl@gmail.com>
2025-03-16 16:43:14 +0200

    singleton1

---
+++ src/Singleton/SingletonRegistry/main/Main.java
@@ -0,0 +1,14 @@
+package Singleton.SingletonRegistry.main;
+
+import Singleton.SingletonRegistry.classes.Pachet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pachet pachet = Pachet.getInstance("Lisbon",5000);
+        System.out.println(pachet);
+
+        Pachet pachet1 = Pachet.getInstance("Lisbon", 4200);
+        System.out.println(pachet1);
+    }
+}
